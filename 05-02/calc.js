cb09f81fd65287629855f24897749c1e5765e6f9
Aluno <ramosandrey05@gmail..com>
2025-02-05 15:45:15 -0300

    realçando

---
+++ 05-02/calc.js
@@ -0,0 +1,9 @@
+// calculo
+let num1 = Number(prompt("digite um numero"));
+let num2 = Number(prompt("digite um novo numero"));
+
+console.log(typeof num1);
+console.log(typeof num2)
+
+// resposta
+console.log(`a soma é ${num1 + num2}`);
