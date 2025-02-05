aadb01c6fdec2475f43cdcd523b3eff125fb2074
Aluno <ramosandrey05@gmail..com>
2025-02-05 14:54:55 -0300

    javascrit bro

---
+++ 05-02/scrip.js
@@ -0,0 +1,11 @@
+// testes e só isso
+console.log("teste");
+document.write("bem vindo ao seina");
+let nome = prompt("qual é o seu nome?");
+let idade = prompt("qual é sua idade?");
+let genero = prompt("como você se identifica?")
+// documents
+document.write("<br>nome:" + nome);
+document.write("<br>idade:" + idade);
+document.write("<br><br>");
+document.write(`seja bem vindo: ${nome}, sua idade é: ${idade} anos, seu genero: ${genero}. `);
