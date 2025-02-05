c36e1977322d0ccf0f661be5f998a8378397f398
Aluno <ramosandrey05@gmail..com>
2025-02-05 16:19:06 -0300

    real

--- 05-02/calc.js
+++ 05-02/calc.js
@@ -1,9 +1,25 @@
 // calculo
-let num1 = Number(prompt("digite um numero"));
-let num2 = Number(prompt("digite um novo numero"));
+// let num1 = Number(prompt("digite um numero"));
+// let num2 = Number(prompt("digite um novo numero"));
+// let num3 = Number(prompt("digite de novo"));
+// let num4 = Number(prompt("digite again"));
 
-console.log(typeof num1);
-console.log(typeof num2)
+// operação
+// let soma = num1 + num2;
+// let sub =  num3 - num4;
+// let mult = num1 * num2;
+// let div = num1 / num4;
 
 // resposta
-console.log(`a soma é ${num1 + num2}`);
+// console.log(`a soma é ${soma}`);
+// console.log(`a subtração é ${sub}`)
+// console.log(`a multiplicação é ${mult}`)
+// console.log(`a divisão é ${div}`)
+
+// exercio
+let tom = ("tom");
+let FirstTom = ("hanks");
+let SecondTom = ("cruise");
+let myFavoriteTom = prompt("qual é seu favorito tom cruise ou tom hanks?");
+
+console.log = (`o seu favorito é: ${tom} ${myFavoriteTom}`);
