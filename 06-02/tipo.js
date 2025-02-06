e75a38c43ac6e94b414346c3c96e9a8cee0de6e9
Aluno <ramosandrey05@gmail..com>
2025-02-06 14:41:45 -0300

    mermão

---
+++ 06-02/tipo.js
@@ -0,0 +1,16 @@
+// string
+let nome = "andrey"
+
+// inteiro
+let idade = 19;
+ 
+// booleado
+let casado = false;
+
+// numeros futuantes
+let valorproduto = 5.35;
+
+console.log("Meu noem é", nome);
+console.log("iade:", idade);
+console.log("casado?" + casado);
+console.log("R$", valorproduto);
---
+++ 06-02/variavel.js
@@ -0,0 +1,10 @@
+let mensagem = "OLÁ mundo";
+let idade = "tenho 19 anos";
+const nome = "andrey ramos dos santos";
+
+// idade
+
+idade = 38;
+
+console.log(mensagem);
+console.log(`${mensagem} e eu ${idade}`);
