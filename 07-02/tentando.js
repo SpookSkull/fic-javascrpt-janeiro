b57a3f445c8b8da9f7603c82996e380bbc5879d1
Aluno <ramosandrey05@gmail..com>
2025-02-07 14:14:17 -0300

    novo

---
+++ 07-02/tentando.js
@@ -0,0 +1,9 @@
+let mesagem = "olá mundo";
+let idade = 36;
+let casado = true;
+let times = ["paulista", "santos", 12, true];
+ 
+console.log("variavel1", mesagem);
+console.log("variavel2", idade);
+console.log(casado);
+console.log(times[0])
