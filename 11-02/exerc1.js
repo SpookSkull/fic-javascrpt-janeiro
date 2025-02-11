6c256bcbba7f6900d15f0106f5c9d6964129dde6
Aluno <ramosandrey05@gmail..com>
2025-02-11 15:39:33 -0300

    bem vindo ao exercicio ma feito por minha parte

---
+++ 11-02/exerc1.js
@@ -0,0 +1,9 @@
+const fruits = ["morango", "banana", "mamão", "pera"];
+
+ if (fruits.includes("abacaxi")) {
+    console.log("abacaxi existe");
+  } else if (fruits.includes("pera")) {
+    console.log("pera existe");
+  } else {
+    console.log("são uma mentira")
+  }
