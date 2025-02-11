d3dd3e6f22f23841bb3fa9865815a321b44f6f94
Aluno <ramosandrey05@gmail..com>
2025-02-11 16:17:39 -0300

    fim

---
+++ 11-02/exerc2.js
@@ -0,0 +1,11 @@
+let idade = 5;
+
+let minima = 7;
+
+let maxima = 65;
+
+if (idade > maxima || idade < minima) {
+    console.log("sua entrada é grátis");
+} else {
+    console.log("pague R$30,00 para entrar");
+}
---
+++ 11-02/valor2.js
@@ -0,0 +1,7 @@
+let valor = 25;
+
+ if (valor < 10 || valor > 20 ) {
+    console.log("esta valor é valido")
+ } else {
+    console.log("não é valido")
+ }
