f90718cf6093bb47459114be98e5d7dc66bcd4d8
Aluno <ramosandrey05@gmail..com>
2025-02-12 14:54:48 -0300

    exercicio blz

---
+++ 12-02/exerc2.js
@@ -0,0 +1,8 @@
+const cities = [`são paulo`, `rio de janeiro`, `minas gerais`]
+
+let upperCaseCities = []
+
+for (let i = 0; i < cities.length; i++) {
+    upperCaseCities.push(cities[i].toUpperCase());
+}
+ console.log(upperCaseCities);
