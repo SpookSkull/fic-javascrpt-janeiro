2c0be058b72c75491bc33ce675ad6748c2bc3e68
Aluno <ramosandrey05@gmail..com>
2025-02-13 14:42:52 -0300

    função agora

---
+++ 13-02/func.js
@@ -0,0 +1,6 @@
+function nomeFuncao(arg1, arg2){
+
+    
+    return arg1 + arg2
+}
+console.log(nomeFuncao(5,4));
--- 13-02/while.js
+++ 13-02/while.js
@@ -6,3 +6,9 @@ while (multiplicador <= 10) {
     document.write("7 *" + multiplicador + " = " + 7 * multiplicador + "<br>");
   multiplicador += 1;
 }
+
+
+function nomeFuncao(arg1, arg2){
+    return arg1 + arg2
+}
+console.log(nomeFuncao(5,4));
