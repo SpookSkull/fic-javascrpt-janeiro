99ca7a0da25ffd8604d64de33789ffe952c8030e
Aluno <ramosandrey05@gmail..com>
2025-02-13 16:16:32 -0300

    tentando esta coisa com a lapiseira quebrada

---
+++ 13-02/func2.js
@@ -0,0 +1,21 @@
+function mostrarnome(nome) {
+    document.write(nome);
+}
+
+    function mostrarfrase() {
+        document.write("")
+}
+
+
+
+function pularlinha() {
+    document.write("<br><br>");
+}
+
+
+pularlinha();
+mostrarnome("fabio");
+pularlinha();
+mostrarnome("caio");
+pularlinha();
+mostrarnome("carla");
