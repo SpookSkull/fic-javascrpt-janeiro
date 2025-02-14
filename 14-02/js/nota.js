7dbad50dca02da3b818c1cfa778158fbfe4153e6
Aluno <ramosandrey05@gmail..com>
2025-02-14 15:49:15 -0300

    estranho

---
+++ 14-02/js/nota.js
@@ -0,0 +1,9 @@
+
+// variaveis
+const inputnota = document.getElementById("nota");
+const resultado = document.getElementById("resultado")
+
+function resultadonotaclick() {
+    console.log("ola andrey");
+    alert("ola mermão");
+}
