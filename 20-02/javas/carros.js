f6fa4a4345acead7b9d17d425da59c74bb0886f7
Aluno <ramosandrey05@gmail..com>
2025-02-21 14:44:19 -0300

    carros em objetos feios

---
+++ 20-02/javas/carros.js
@@ -0,0 +1,15 @@
+const cars = [
+    {
+        brand: "toyota",
+        model: "corola",
+        year: 2022
+    },
+    {
+        brand: "volkswagen",
+        model: "sivicus",
+        year: 2024
+    }
+]
+console.log(cars[0].model)
+console.log(cars[1].year)
+console.log(cars[0].year)
