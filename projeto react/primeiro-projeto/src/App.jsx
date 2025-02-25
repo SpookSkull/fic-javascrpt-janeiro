82e9148725a12f649ce9d4e64889576e3668f061
Aluno <ramosandrey05@gmail..com>
2025-02-25 16:07:20 -0300

    realmente dificil

--- projeto react/primeiro-projeto/src/App.jsx
+++ projeto react/primeiro-projeto/src/App.jsx
@@ -1,11 +1,19 @@
+import header from "./Components/header";
 
 function App() {
 
   return (
     <>
-    <p>
-      Andrey 
-    </p>
+    <div className="container">
+      
+      <h1>seja bem vindo</h1>
+
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae facere quis corrupti, laudantium aliquam velit nemo harum.
+         Labore, similique porro fugit ad fugiat exercitationem impedit laudantium officia esse praesentium illum?
+      </p>
+
+    </div>
     </>
   )
 }
